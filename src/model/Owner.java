c184fa51b262340dfee7a13877d57e6ec5cf910a
Ethan McCue <ethan@mccue.dev>
2026-04-30 16:17:41 -0400

    There is no reason to ever have the pets arraylist be null

--- src/model/Owner.java
+++ src/model/Owner.java
@@ -17,27 +17,20 @@ public class Owner {
     private  String name;
     private String email;
     private int phoneNumber;
-    private ArrayList<Pet> pets;
+    private ArrayList<Pet> pets = new ArrayList<>();
 
 
     public void addPet(Pet pet) {
-        if (pets == null) {
-            pets = new ArrayList<>();
-        }
         pets.add(pet);
         pet.setOwner(this);
     }
 
     public void removePet(Pet pet) {
-        if (pets != null) {
-            pets.remove(pet);
-            pet.setOwner(null);
-        }
+        pets.remove(pet);
+        pet.setOwner(null);
     }
 
     public Pet findPetByName(String name) {
-        if (pets == null) return null;
-
         for (Pet p : pets) {
             if (p.getName().equalsIgnoreCase(name)) {
                 return p;
